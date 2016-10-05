ee8531eae71c82d76e945222a20db2be212635b7
Austin Willis <austinwillis8@gmail.com>
2016-10-05 02:55:27 +0000

    moved markov to single file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 
-var markov = require('../node-markov-master');
+var markov = require('../');
 
 var compliments = `You have very smooth hair
 You deserve a promotion
